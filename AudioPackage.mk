cfef2e04e652388154213e64cf29c6ff2a7e61c4
Yagya Parihar <yagya.parihar@gmail.com>
2021-04-26 19:44:57 -0600

    Update the sounds stuff.
    Most of these probably didn't need changes, but I did them anyway for good measure. Just making sure no old sounds slip through the cracks.

--- AudioPackage.mk
+++ AudioPackage.mk
@@ -1,53 +1,32 @@
-LOCAL_PATH := device/lge/occam/sounds/
+#
+# Audio Package 13 - L
+#
+# Include this file in a product makefile to include these audio files
+#
+#
 
-AUDIO_FILES := \
-	ringtones/Ganymede.ogg \
-	ringtones/Pyxis.ogg \
-	ringtones/Oberon.ogg \
-	ringtones/Titania.ogg \
-	ringtones/Phobos.ogg \
-	ringtones/Triton.ogg \
-	ringtones/Dione.ogg \
-	ringtones/Umbriel.ogg \
-	ringtones/Luna.ogg \
-	ringtones/Callisto.ogg \
-	ringtones/Sedna.ogg \
-	ringtones/Atria.ogg \
-	alarms/Helium.ogg \
-	alarms/Carbon.ogg \
-	alarms/Krypton.ogg \
-	alarms/Argon.ogg \
-	alarms/Timer.ogg \
-	alarms/Neon.ogg \
-	alarms/Osmium.ogg \
-	alarms/Oxygen.ogg \
-	alarms/Platinum.ogg \
-	notifications/Salacia.ogg \
-	notifications/Rhea.ogg \
-	notifications/Elara.ogg \
-	notifications/Europa.ogg \
-	notifications/Carme.ogg \
-	notifications/Titan.ogg \
-	notifications/Ariel.ogg \
-	notifications/Io.ogg \
-	notifications/Ceres.ogg \
-	notifications/Iapetus.ogg \
-	notifications/Tethys.ogg \
+LOCAL_PATH := device/lge/occam
 
+# Simple files that do not require renaming
+ALARM_FILES := Argon Carbon Helium Krypton Neon Oxygen Osmium Platinum Timer
 
-AUDIO_FILES += \
-	ui/audio_end.ogg \
-	ui/audio_initiate.ogg \
-	ui/NFCTransferComplete.ogg \
-	ui/NFCFailure.ogg \
-	ui/NFCTransferInitiated.ogg \
-	ui/NFCInitiated.ogg \
-	ui/NFCSuccess.ogg \
+NOTIFICATION_FILES := Ariel Ceres Carme Elara Europa Iapetus Io Rhea Salacia Titan Tethys
 
-#	ui/VideoStop.ogg \
+RINGTONE_FILES := Atria Callisto Dione Ganymede Luna Oberon Phobos Pyxis Sedna Titania Triton \
+	Umbriel
 
-PRODUCT_COPY_FILES := $(foreach fn,$(AUDIO_FILES),\
-	$(LOCAL_PATH)/$(fn):system/media/audio/$(fn))
+EFFECT_FILES := Effect_Tick KeypressReturn KeypressInvalid KeypressDelete KeypressSpacebar KeypressStandard \
+	camera_focus Dock Undock Lock Unlock Trusted ChargingStarted InCallNotification \
+	camera_click VideoRecord WirelessChargingStarted LowBattery VideoStop
 
-# use ui effect from aosp
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage13.mk)
+PRODUCT_COPY_FILES += $(foreach fn,$(ALARM_FILES),\
+	$(LOCAL_PATH)/alarms/$(fn).ogg:system/media/audio/alarms/$(fn).ogg)
+
+PRODUCT_COPY_FILES += $(foreach fn,$(NOTIFICATION_FILES),\
+	$(LOCAL_PATH)/notifications/$(fn).ogg:system/media/audio/notifications/$(fn).ogg)
+
+PRODUCT_COPY_FILES += $(foreach fn,$(RINGTONE_FILES),\
+	$(LOCAL_PATH)/ringtones/$(fn).ogg:system/media/audio/ringtones/$(fn).ogg)
+
+PRODUCT_COPY_FILES += $(foreach fn,$(EFFECT_FILES),\
+	$(LOCAL_PATH)/ui/$(fn).ogg:system/media/audio/ui/$(fn).ogg)
